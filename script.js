bfcb39d31f41d0fdab8691d27220622d051a7067
Gustavo Moura <gsilvamoura@hotmail.com>
2023-06-02 20:57:50 -0300

    Páginas adicionais incluidas

---
+++ script.js
@@ -0,0 +1,29 @@
+function sub1() {
+    let subl = document.getElementById("navbar1");
+    subl.style.textDecoration = 'underline' ;
+  }
+  
+  function removeSub1() {
+    let subl = document.getElementById("navbar1");
+    subl.style.textDecoration = 'none' ;
+  }
+
+function sub2() {
+    let subl = document.getElementById("navbar2");
+    subl.style.textDecoration = 'underline' ;
+  }
+  
+  function removeSub2() {
+    let subl = document.getElementById("navbar2");
+    subl.style.textDecoration = 'none' ;
+  }
+
+  function sub3() {
+    let subl = document.getElementById("navbar3");
+    subl.style.textDecoration = 'underline' ;
+  }
+  
+  function removeSub3() {
+    let subl = document.getElementById("navbar3");
+    subl.style.textDecoration = 'none' ;
+  }
